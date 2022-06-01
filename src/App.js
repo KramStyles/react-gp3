ef3f692d49a0dc4362a484cdcdeebdde2dd1f2f0
kramstyles <mark.eke@decagon.dev>
2022-06-01 15:58:10 +0100

    added components and containers to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import { Article } from "./components";
+import { Article, Navbar, CTA, Brand, Feature } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatGPT3,
+} from "./containers";
 
 const App = () => {
   return (
-    <div>
-      <Article />
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
