220618c715d3985cc400fe7668da1e601b2f4464
kramstyles <mark.eke@decagon.dev>
2022-06-08 21:22:46 +0100

    added gradient heading

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -1,7 +1,13 @@
 import "./style.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gp_header section_padding" id="home">
+      <div className="gp_header-content">
+        <h1 className="gradient_text">Let's Build Something amazing with GPT OpenAI</h1>
+      </div>
+    </div>
+  )
 };
 
 export default Header;
